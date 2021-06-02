dc5e4e514fcb584c56a610d9b029299043161f7f
CoderAKS07 <79311937+CoderAKS07@users.noreply.github.com>
2021-06-02 18:26:49 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,66 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+
+    //write code to change the background color here
+    background("yellow");
+
+    //write code to show a heading for showing the result of Quiz
+    textSize(30);
+
+    //call getContestantInfo( ) here
+
+    //write condition to check if contestantInfor is not undefined
+    if(allContestants !== undefined)
+    {
+      fill("blue");
+      textSize(20);
+      text("Result of The Quiz", 400, 50);
+      //write code to add a note here
+      text("*Note : The Name marked in Green is the winner!!!", 130, 230);
+    }
+    //write code to highlight contest who answered correctly
+    for(var plr in allContestants)
+    {
+      var correctAns = "2";
+      if(correctAns === allContestans[plr].answers)
+      {
+        fill("green");
+      }
+      else
+      {
+        fill("red");
+      }
+    }
+  }
+
+}
